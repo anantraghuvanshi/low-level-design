e85c63943890742ba3c65ee9d3e18bfc2b7bc8f3
Anant <anantsinghraghuvanshi1@gmail.com>
2024-05-15 14:17:41 +0530

    files added

---
+++ src/api/GameEngine.java
@@ -0,0 +1,20 @@
+package api;
+
+import game.Board;
+import game.GameResult;
+import game.Move;
+import game.Player;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class GameEngine {
+    public Board start(){
+        return new Board();
+    }
+    public void move(Board board, Player player, Move move){
+
+    }
+    public GameResult isComplete(Board board){
+
+    }
+}
---
+++ src/boards/TicTacToe.java
@@ -0,0 +1,6 @@
+package boards;
+
+import game.Board;
+
+public class TicTacToe extends Board {
+}
